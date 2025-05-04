384701db1603170f8dce75ba728cd9d67f8e5307
XarlosZakk <carlosgamer1009@gmail.com>
2025-05-04 12:28:13 -0500

    ejercicio de js

--- backend/Resource/array_Reestructuracion.js
+++ backend/Resource/array_Reestructuracion.js
@@ -1,1 +1,16 @@
 // reestructuracion de array
+var a = 10;
+function cambiarValor(){
+    var a = 15;
+    return a;
+}
+
+a = cambiarValor(a);
+console.log(a);
+
+
+// respuestas:
+// 1) 10
+// 2) 15
+// 3) undefined
+// 4) Error
